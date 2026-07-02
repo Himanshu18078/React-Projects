ce45ae064ea33a5d19ad016b586f69669d5c75fe
Himanshu18078 <himanshubisht18078@gmail.com>
2026-07-03 01:17:36 +0530

    Impoved UI and Added Delete Functionality

--- Social Media/social-media/src/store/post-list-store.jsx
+++ Social Media/social-media/src/store/post-list-store.jsx
@@ -1,42 +1,66 @@
 import { createContext, useReducer } from "react";
+
 export const PostList = createContext({
+  // The Following are the default values
   postList: [],
-  addPost: () => { },
-  deletePost: () => { },
+  addPost: () => {},
+  deletePost: () => {},
 });
-const postListReducer = (currentPost, action) => {
-  return currentPost;
-}
+
+const postListReducer = (currentPostList, action) => {
+  let newPostList = currentPostList
+  if(action.type === 'DELETE_POST'){
+    newPostList = currentPostList.filter(post => post.id !== action.payLoad.postId)
+  }
+  return newPostList;
+};
+
 const PostListProvider = ({ children }) => {
+
   const [postList, dispatchPostList] = useReducer(postListReducer, DEFAULT_POST_LIST);
 
-  const addPost = () => { };
-  const deletePost = () => { };
+  const addPost = () => {};
 
-  return (<PostList.Provider value={
-    {
-      postList: postList,
-      addPost: addPost,
-      deletePost: deletePost,
-    }
-  }>
-    {children}
-  </PostList.Provider>)
-}
-const DEFAULT_POST_LIST = [{
-  id: '1',
-  title: 'Going to Mumbai',
-  body: 'Hi friends , I am going to Mumbai for my vacations. Hope to enjoy a lot. Peace Lot',
-  reactions: 2,
-  userId: 'user-9',
-  tags: ['Vacations', 'Mumbai', 'Enjoying']
-},
-{
-  id: '2',
-  title: 'Pass Ho Gye Bhai',
-  body: '4 saal ki masti ke bad bhi ho gye hai pass. Hard to bellieve',
-  reactions: 15,
-  userId: 'user-12',
-  tags: ['Graduating', 'Unbelievable']
-}];
+  const deletePost = (postId) => {
+    dispatchPostList({
+      type : 'DELETE_POST',
+      payLoad : {
+        postId
+      }
+    })
+  };
+
+  
+  return (
+    <PostList.Provider
+      value={{
+        postList: postList,
+        addPost: addPost,
+        deletePost: deletePost,
+      }}
+    >
+      {children}
+    </PostList.Provider>
+  );
+};
+
+const DEFAULT_POST_LIST = [
+  {
+    id: '1',
+    title: 'Going to Mumbai',
+    body: 'Hi friends , I am going to Mumbai for my vacations. Hope to enjoy a lot. Peace Lot',
+    reactions: 2,
+    userId: 'user-9',
+    tags: ['Vacations', 'Mumbai', 'Enjoying'],
+  },
+  {
+    id: '2',
+    title: 'Pass Ho Gye Bhai',
+    body: '4 saal ki masti ke bad bhi ho gye hai pass. Hard to bellieve',
+    reactions: 15,
+    userId: 'user-12',
+    tags: ['Graduating', 'Unbelievable'],
+  },
+];
+
 export default PostListProvider;
